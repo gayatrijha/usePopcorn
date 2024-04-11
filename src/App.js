d07159933c9aabc6464795a5c377b535e55eb629
Gayatri Jha <jha.gayatri01@gmail.com>
2024-04-11 06:11:38 +0530

    pagetitle

--- src/App.js
+++ src/App.js
@@ -319,6 +319,13 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     },
     [selectedId]
   );
+  useEffect(
+    function () {
+      if (!title) return;
+      document.title = `Movie | ${title}`;
+    },
+    [title]
+  );
   return (
     <div className="details">
       {isLoading ? (
